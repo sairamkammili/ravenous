c14876236ab03c0d60096e89e13994f463e2726c
Sairam <sairamkammili@gmail.com>
2018-10-04 21:10:08 -0700

    master copy

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+// import BusinessList from './components/BusinessList';
+// import SearchBar from './components/SearchBar';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <h1>ravenous</h1>
+      {/* <h1>{BusinessList}</h1>
+      <h1>{SearchBar}</h1> */}
+    </div>
+    );
+  }
+}
+
+export default App;
