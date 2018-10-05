ceb4e5b2b928fee97683655393fb6f229adaf4a0
Sairam <sairamkammili@gmail.com>
2018-10-04 21:53:41 -0700

    imported components and facing import errors while retrieving

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import BusinessList from './components/BusinessList';
-// import SearchBar from './components/SearchBar';
+import  BusinessList from '../components/BusinessList';
+import { SearchBar } from '../components/SearchBar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
       <h1>ravenous</h1>
-      {/* <h1>{BusinessList}</h1>
-      <h1>{SearchBar}</h1> */}
+      <BusinessList />
+      <SearchBar />
     </div>
     );
   }
